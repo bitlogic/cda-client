413fb28b2ab0734ba7ed8724139af76f9bd93f2f
nicolo.pisano <nicolo.pisano@proveedor.ues21.edu.ar>
2021-01-28 17:00:39 -0300

    Comment MongoDB

--- fingerprint_sync.py
+++ fingerprint_sync.py
@@ -17,27 +17,27 @@ def authenticate():
 def sync():
     check_inactive_users()
 
-    local_db.fingerprints.drop()
-    fingerprints = db.reference('fingerprints/').get()
-    for i in fingerprints:
-        add_fingerprint(i, fingerprints[i]['user'])
-    print('Finish fingerprint sync')
+    # local_db.fingerprints.drop()
+    # fingerprints = db.reference('fingerprints/').get()
+    # for i in fingerprints:
+    #     add_fingerprint(i, fingerprints[i]['user'])
+    # print('Finish fingerprint sync')
 
-    local_db.users.drop()
-    users = db.reference('users/').get()
-    for i in users:
-        add_user(i, users[i]['name'], users[i]['lastname'], users[i]['company'], users[i]['status'])
-    print('Finish user sync')
+    # local_db.users.drop()
+    # users = db.reference('users/').get()
+    # for i in users:
+    #     add_user(i, users[i]['name'], users[i]['lastname'], users[i]['company'], users[i]['status'])
+    # print('Finish user sync')
 
-    data = {'date': datetime.datetime.now()}
-    local_db.syncs.insert_one(data)
-    print('Change sync date')
+    # data = {'date': datetime.datetime.now()}
+    # local_db.syncs.insert_one(data)
+    # print('Change sync date')
 
 
 def check_inactive_users():
     inactive_users = db.reference('users/').order_by_child('status').equal_to('INACTIVE').get()
     position_numbers = []
-    all_position_numbers = []
+
   
     for iu in inactive_users:
         user_fingerprints = db.reference('fingerprints/').order_by_child('user').equal_to(iu).get()
@@ -71,33 +71,33 @@ def check_inactive_users():
             )
 
         # Decrease all position_numbers of inactive users by 1
-        
+
         position_numbers = [x - 1 for x in position_numbers]
 
     
 
 
-def add_user(user_id, name, lastname, company, status):
-    data = {
-        '_id': user_id,
-        'name': name,
-        'lastname': lastname,
-        'status': status,
-        'company': company
-    }
-    local_db.users.insert_one(data)
+# def add_user(user_id, name, lastname, company, status):
+#     data = {
+#         '_id': user_id,
+#         'name': name,
+#         'lastname': lastname,
+#         'status': status,
+#         'company': company
+#     }
+#     local_db.users.insert_one(data)
 
 
-def add_fingerprint(fingerprint_id, user_id):
-    data = {
-        'fingerprint': fingerprint_id,
-        'user': user_id
-    }
-    local_db.fingerprints.insert_one(data)
+# def add_fingerprint(fingerprint_id, user_id):
+#     data = {
+#         'fingerprint': fingerprint_id,
+#         'user': user_id
+#     }
+#     local_db.fingerprints.insert_one(data)
 
 
-client = MongoClient('localhost', 27017)
-local_db = client['cda']
+# client = MongoClient('localhost', 27017)
+# local_db = client['cda']
 
 authenticate()
 sync()
