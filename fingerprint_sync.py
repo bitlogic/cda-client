3973236fc7c8f27019c2215d5f984b12c48d87c4
nicolo.pisano <nicolo.pisano@proveedor.ues21.edu.ar>
2021-02-10 11:33:27 -0300

    split sync and delete

--- fingerprint_sync.py
+++ fingerprint_sync.py
@@ -15,8 +15,6 @@ def authenticate():
 
 
 def sync():
-    
-    
 
     local_db.fingerprints.drop()
     fingerprints = db.reference('fingerprints/').get()
@@ -33,68 +31,6 @@ def sync():
     data = {'date': datetime.datetime.now()}
     local_db.syncs.insert_one(data)
     print('Change sync date')
-
-
-def delete_inactive_fingerprints():
-    
-    
-    inactive_users = db.reference('users/').order_by_child('status').equal_to('INACTIVE').get()
-    position_numbers = []
-  
-    for iu in inactive_users:
-        user_fingerprints = db.reference('fingerprints/').order_by_child('user').equal_to(iu).get()
-
-        for fing in user_fingerprints:
-            position_numbers.append(user_fingerprints[fing]['position_number'])
-    
-    position_numbers.sort()
-   
-    for position_number in position_numbers:
-
-       
-        print(position_number)
-
-         # Borra en el sensor
-
-
-        if delete(position_number):
-            print('Fingerprints deleted in sensor')
-
-        else:
-            print('Error deleting fingerprints in sensor')
-            return
-
-        # Borra en Firebase
-        
-        fingerprintdata = db.reference('fingerprints/').order_by_child('position_number').equal_to(position_number).get()
-        for key in fingerprintdata:
-            db.reference('fingerprints/').child(key).delete()
-         
-
-        # Reduce de 1 todos los siguientes all position_numbers (todos, no solo los inactivos)
-
-        # next_fingerprints = db.reference('fingerprints/').order_by_child('position_number').start_at(position_number).get()
-        # for key, value in next_fingerprints.items():
-           
-        #     db.reference('fingerprints/').child(key).update(
-        #         {
-        #             'position_number': value['position_number'] -1
-        #         }
-        #     )
-        # # Reduce los position_numbers de -1
-        
-        # position_numbers = [x - 1 for x in position_numbers]
-    
-
-def delete_inactive_users():
-
-    inactive_users = db.reference('users/').order_by_child('status').equal_to('INACTIVE').get()
-    for key in inactive_users:
-        db.reference('users/').child(key).update(
-                {
-                    'status': "DELETED"
-                }
-            )
 
 
 
---
+++ main-delete-fingerprints.py
@@ -0,0 +1,77 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+import datetime
+from os import system, name
+from delete_fingerprint import delete
+
+
+def authenticate():
+    cred = credentials.Certificate("google_credentials.json")
+    firebase_admin.initialize_app(cred, options={
+        'databaseURL': 'https://cda-bithouse.firebaseio.com/',
+    })
+def delete_inactive_fingerprints():
+    
+    
+    inactive_users = db.reference('users/').order_by_child('status').equal_to('INACTIVE').get()
+    position_numbers = []
+  
+    for iu in inactive_users:
+        user_fingerprints = db.reference('fingerprints/').order_by_child('user').equal_to(iu).get()
+
+        for fing in user_fingerprints:
+            position_numbers.append(user_fingerprints[fing]['position_number'])
+    
+    position_numbers.sort()
+   
+    for position_number in position_numbers:
+
+       
+        print(position_number)
+
+         # Borra en el sensor
+
+
+        if delete(position_number):
+            print('Fingerprints deleted in sensor')
+
+        else:
+            print('Error deleting fingerprints in sensor')
+            return
+
+        # Borra en Firebase
+        
+        fingerprintdata = db.reference('fingerprints/').order_by_child('position_number').equal_to(position_number).get()
+        for key in fingerprintdata:
+            db.reference('fingerprints/').child(key).delete()
+         
+
+        # Reduce de 1 todos los siguientes all position_numbers (todos, no solo los inactivos)
+
+        # next_fingerprints = db.reference('fingerprints/').order_by_child('position_number').start_at(position_number).get()
+        # for key, value in next_fingerprints.items():
+           
+        #     db.reference('fingerprints/').child(key).update(
+        #         {
+        #             'position_number': value['position_number'] -1
+        #         }
+        #     )
+        # # Reduce los position_numbers de -1
+        
+        # position_numbers = [x - 1 for x in position_numbers]
+    
+
+def delete_inactive_users():
+
+    inactive_users = db.reference('users/').order_by_child('status').equal_to('INACTIVE').get()
+    for key in inactive_users:
+        db.reference('users/').child(key).update(
+                {
+                    'status': "DELETED"
+                }
+            )
+system('clear')
+authenticate()
+delete_inactive_fingerprints()
+delete_inactive_users()
